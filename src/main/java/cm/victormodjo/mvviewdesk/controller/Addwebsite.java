6ec3d16493be3333c7d2910bf60da15e3d95398b
Devmodjo <yvankamsu88@gmail.com>
2025-06-09 04:34:14 +0100

    onfiguration de l'interface d'enregistrement

---
+++ src/main/java/cm/victormodjo/mvviewdesk/controller/Addwebsite.java
@@ -0,0 +1,22 @@
+package cm.victormodjo.mvviewdesk.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Addwebsite {
+
+    @FXML
+    private TextArea descript;
+
+    @FXML
+    private TextField link;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private AnchorPane register;
+
+}
--- src/main/java/cm/victormodjo/mvviewdesk/controller/MainappController.java
+++ src/main/java/cm/victormodjo/mvviewdesk/controller/MainappController.java
@@ -1,4 +1,15 @@
 package cm.victormodjo.mvviewdesk.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 public class MainappController {
+
+    @FXML
+    private Button addwebsite;
+
+    @FXML
+    private VBox vboxlistwebsite;
+
 }
